2d4d76aed949382390fac9dbfcca145e6dd7aa6b
Welcome <Welcome@HP-Pavilion>
2021-01-18 19:02:45 +0530

    first commit on git

---
+++ GitDemoTest/src/test/java/com/demo/test/DemoTest.java
@@ -0,0 +1,19 @@
+package com.demo.test;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class DemoTest {
+
+public static Properties prop;
+	
+	public DemoTest(){
+		try {
+			prop= new Properties();
+		FileInputStream ip=new FileInputStream("/Users/Welcome/eclipse-workspace/BpaAutfraTest/src/main/java/com/bpa/qa/config/config.properties");
+			prop.load(ip);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		}
+}
